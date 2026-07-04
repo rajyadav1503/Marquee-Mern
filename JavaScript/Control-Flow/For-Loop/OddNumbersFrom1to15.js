f18948e14067b9ef4baa5199dfb8967656156eca
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-04 13:40:06 +0530

    oddNumbers

---
+++ JavaScript/Control-Flow/For-Loop/OddNumbersFrom1to15.js
@@ -0,0 +1,12 @@
+// Print all the odd numbers till 15
+
+function oddNumbers () {
+
+    for( let i = 0 ; i <= 15 ;i++ ) {
+        if(i % 2 != 0) {
+            console.log(i);
+        }
+    }
+}
+
+oddNumbers();
