a76c231c2ffaee9ce1abe07d2ddd83c3037c7570
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-04 13:25:32 +0530

    table

---
+++ JavaScript/Control-Flow/For-Loop/table.js
@@ -0,0 +1,14 @@
+// Table of a number using for loop
+
+function table (number) {
+
+    for(let i=1 ; i<=10 ; i++){
+
+        console.log(`${number} * ${i} = ` + number * i);
+    
+    }
+}
+
+let num = 2;
+
+table(num);
