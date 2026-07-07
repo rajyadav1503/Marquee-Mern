34bc2c41592b69726278db3a608a62ce65642ed3
rajyadav1503 <raajyadav1503@gmail.com>
2026-07-07 13:45:51 +0530

    generatorFunction

---
+++ JavaScript/Functions/GeneratorFunction.js
@@ -0,0 +1,10 @@
+function* simpleGenerator() {
+    yield 'Hello';
+    yield 'World';
+}
+
+const iterator = simpleGenerator();
+console.log(iterator.next(123));
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
